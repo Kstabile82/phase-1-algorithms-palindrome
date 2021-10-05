e4181ed7d1e2d79c7dd53bb3e82de2031f72fbce
KarinaStabile <karina.stabile@gmail.com>
2021-10-04 22:16:35 -0400

    commit tree 3cbae5a891ebb7bbb63e4d862367509750673695

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let wordArr = [...word];
+  let forwardArr = []; 
+  let backwardArr = []; 
+  for (let i = wordArr.length-1; i >= 0; i--) {
+    backwardArr.push(wordArr[i]); 
+    }
+    if (wordArr === backwardArr) {
+      return true;
+    }
+    else {
+      return false;
+    }
+//   forwardArr.map(idxA => {
+//     backwardArr.map(idxB => {
+//     if (idxA === idxB) {
+//       return true;
+//     }
+//     else {
+//       return false;
+//     }
+//   })
+// })
 }
 
 /* 
   Add your pseudocode here
+  //convert string to array
+  //make two empty arrays, frontArray and backArray
+  //need two iterations, for loops, one starts at front one starts at end
+  iterate through arrays, push each index into new array. 
+  //conditional: if front array === back array, is true; else is false
 */
 
 /*
